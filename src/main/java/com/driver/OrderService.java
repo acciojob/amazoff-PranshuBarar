9cd1b6014b19520122bd92acf9bd76b0b41efb34
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-02-11 14:49:20 +0530

    Solution3

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 @Service
 public class OrderService {
-    @Autowired
-    OrderRepository orderRepository;
+//    @Autowired
+//    OrderRepository orderRepository;
+
+    OrderRepository orderRepository = new OrderRepository();
 
 
 
